50e56189cf70a7636e5547a383781bdb57b8e7d3
Balu567 <balarajumalla.9@gmail.com>
2021-11-23 12:21:32 +0530

    schema,resolver

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-    price: String
-  }
+const typeDefs = require("./schema");
 
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+const resolvers = require("./resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
